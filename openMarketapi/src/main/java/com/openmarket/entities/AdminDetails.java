563ec75d1aefd95e94b1fe0bc0ab8353a8885678
jagatjit barik <itsjagatjitbarik@gmail.com>
2022-11-18 22:35:26 +0530

    entity created

--- openMarketapi/src/main/java/com/openmarket/entities/AdminDetails.java
+++ openMarketapi/src/main/java/com/openmarket/entities/AdminDetails.java
@@ -1,5 +1,29 @@
 package com.openmarket.entities;
 
-public class AdminDetails {
+import javax.persistence.Embedded;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.SequenceGenerator;
+
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+
+@Data
+@Entity
+@EqualsAndHashCode(callSuper=false)
+public class AdminDetails extends AbstractUserdata{
+	
+	
+	@SequenceGenerator(name = "admin_sequence_generator", allocationSize = 1, initialValue = 1)
+	@Id
+	@GeneratedValue(generator = "admin_sequence_generator", strategy = GenerationType.SEQUENCE)
+	private Long id;
+	
+	private final String type = "admin";
+	
+	@Embedded
+	AdminLocation location;
 
 }
--- openMarketapi/src/main/java/com/openmarket/entities/CustomerDetails.java
+++ openMarketapi/src/main/java/com/openmarket/entities/CustomerDetails.java
@@ -1,5 +1,32 @@
 package com.openmarket.entities;
 
-public class CustomerDetails {
+import javax.persistence.Embedded;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.SequenceGenerator;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+import lombok.NoArgsConstructor;
+
+@Data
+@EqualsAndHashCode(callSuper = false)
+@Entity
+@NoArgsConstructor
+@AllArgsConstructor
+public class CustomerDetails extends AbstractUserdata{
+	
+	@SequenceGenerator(name = "admin_sequence_generator", allocationSize = 1, initialValue = 1)
+	@Id
+	@GeneratedValue(generator = "admin_sequence_generator", strategy = GenerationType.SEQUENCE)
+	private Long id;
+	
+	private final String type = "user";
+	
+	@Embedded
+	CustomerAddress address;
 
 }
